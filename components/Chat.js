8d8c607830b5b215b721df94e081caceb0ea578b
Ghanshyam Hirani <truptikara@gmail.com>
2022-01-24 18:44:43 +0100

    update

---
+++ components/Chat.js
@@ -0,0 +1,76 @@
+import React, { Component } from 'react';
+import { View, StyleSheet, Platform, KeyboardAvoidingView } from 'react-native';
+import { GiftedChat } from 'react-native-gifted-chat'
+
+export class Chat extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            messages: [],
+        }
+    }
+
+    componentDidMount() {
+        this.setState({
+            messages: [
+                {
+                    _id: 1,
+                    text: 'Hello developer',
+                    createdAt: new Date(),
+                    user: {
+                        _id: 2,
+                        name: 'React Native',
+                        avatar: 'https://placeimg.com/140/140/any',
+                    },
+                },
+            ],
+        })
+    }
+
+    onSend(messages = []) {
+        this.setState(previousState => ({
+            messages: GiftedChat.append(previousState.messages, messages),
+        }))
+    }
+
+    render() {
+        let name = this.props.route.params.name;
+        this.props.navigation.setOptions({ title: name });
+
+        let bgColor = this.props.route.params.bgColor;
+
+        return (
+            <View style={styles.container}>
+                <View
+                    style={{
+                        backgroundColor: bgColor,
+                        width: '100%',
+                        height: '100%',
+                    }}
+                >
+                    <GiftedChat
+                        messages={this.state.messages}
+                        onSend={messages => this.onSend(messages)}
+                        user={{
+                            _id: 1,
+                        }}
+                    />
+                    {Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null
+                    }
+                </View>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    giftedChat: {
+        color: '#000',
+    },
+});
